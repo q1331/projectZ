a5509fe9649f7ae708a71613c98fe21ef40972ca
root <root@ip-172-31-34-52.us-west-2.compute.internal>
2016-06-05 22:51:15 +0000

    Finalized deploy script?

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Install prerequisite
 apt-get update
-apt-get install apt-transport-https ca-certificates
+apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get purge lxc-docker
-apt-get install linux-image-extra-$(uname -r)
-apt-get install apparmor
+apt-get purge -y lxc-docker
+apt-get install -y linux-image-extra-$(uname -r)
+apt-get install -y apparmor
 # Install Docker
 apt-get update
-apt-get install docker-engine
+apt-get install -y docker-engine
 service docker start
 # Install Docker compose
 curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
