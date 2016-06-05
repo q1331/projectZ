8c0b5e6b9e3bd3f55f44df9adf9d11908b4a80e0
q1331 <xts@umich.edu>
2016-06-05 17:24:43 -0500

    Debug deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Install Docker 
-sudo apt-get update && sudo apt-get install docker.io 
+sudo apt-get update && sudo apt-get install -y docker.io 
+# Install Docker compose
+curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 # Go to project dirctory
 cd projectZ
 # Website up
