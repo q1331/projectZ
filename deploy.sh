df9eeab50f841880a620e34c7aeea931877d3a6e
root <root@ip-172-31-34-52.us-west-2.compute.internal>
2016-06-05 22:42:08 +0000

    Debug deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-# Install Docker 
-sudo apt-get update && sudo apt-get install -y docker.io 
+# Install prerequisite
+apt-get update
+apt-get install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get purge lxc-docker
+apt-get install linux-image-extra-$(uname -r)
+apt-get install apparmor
+# Install Docker
+apt-get update
+apt-get install docker-engine
+service docker start
 # Install Docker compose
 curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-# Go to project dirctory
-cd projectZ
 # Website up
-docker-compose up
+docker-compose up -d
 
